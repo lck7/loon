d2f863da6d904e310720bfb9a227399b09f7501d
lck7 <159402507+lck7@users.noreply.github.com>
2024-03-17 05:32:26 +0800

    Update ip-api.js

--- js/ip-api.js
+++ js/ip-api.js
@@ -13,7 +13,7 @@ var requestParams = {
 
 var message = ""
 const paras = ["query","as","isp","country","city","timezone","lon","lat","currency"];
-const paran = ["🗺️远端IP地址","📡远端IPASN","🖥️ASN所属机构","🌍国家/地区","🏘️城市","⏰时区","📍经度","📍纬度","💰货币"];
+const paran = ["🗺️IP地址","📡ASN","🖥️所属机构","🌍国家/地区","🏘️城市","⏰时区","📍经度","📍纬度","💰货币"];
 
 $httpClient.get(requestParams, (error, response, data) => {
     if (error) {
